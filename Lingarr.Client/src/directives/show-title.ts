7c1be99a041d92cf3a775e4879dc3ba8b1e9531c
Rowan Fuchs <rowanfuchs@gmail.com>
2024-10-26 10:26:03 +0200

    fixed typo's and improved texts

--- Lingarr.Client/src/directives/show-title.ts
+++ Lingarr.Client/src/directives/show-title.ts
@@ -6,18 +6,19 @@
 
         const parts = text.split(' - ')
 
-        if (parts.length === 3) {
-            const [showName, episodeNumber, episodeTitle] = parts
-
-            el.innerHTML = `
-                <div class="${originalClasses} inline-flex items-center gap-1.5 overflow-hidden min-w-0 " >
-                    <span class="flex-shrink-0">${showName}</span>
-                    <span>-</span>
-                    <span>${episodeNumber}</span>
-                    <span>-</span>
-                    <span class="text-primary-content/50  truncate block">${episodeTitle}</span>
-                </div>
-            `
+        if (parts.length !== 3) {
+            return
         }
+        const [showName, episodeNumber, episodeTitle] = parts
+
+        el.innerHTML = `
+            <div class="${originalClasses} inline-flex items-center gap-1.5 overflow-hidden min-w-0 " >
+                <span class="flex-shrink-0">${showName}</span>
+                <span>-</span>
+                <span>${episodeNumber}</span>
+                <span>-</span>
+                <span class="text-primary-content/50  truncate block">${episodeTitle}</span>
+            </div>
+        `
     }
 }
