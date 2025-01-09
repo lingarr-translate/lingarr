f0b52525dd40e18aa23e1a56dff03c8294993fad
Rowan <48676736+rowanfuchs@users.noreply.github.com>
2025-01-09 22:44:40 +0100

    Improved subtitle collection and expanded support for caption formats

--- Lingarr.Server/Models/FileSystem/Subtitles.cs
+++ Lingarr.Server/Models/FileSystem/Subtitles.cs
@@ -2,8 +2,9 @@ namespace Lingarr.Server.Models.FileSystem;
 
 public class Subtitles
 {
-    public string? Path { get; set; }
-    public string? FileName { get; set; }
-    public string? Language { get; set; }
-    public string? Format { get; set; }
+    public string Path { get; set; }
+    public string FileName { get; set; }
+    public string Language { get; set; }
+    public string Caption { get; set; }
+    public string Format { get; set; }
 }
