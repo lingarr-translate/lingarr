1d936af7a212164640b0833042e9a303a1209514
Rowan <48676736+rowanfuchs@users.noreply.github.com>
2025-01-31 13:07:07 +0100

    upgraded frontend to latest

--- Lingarr.Client/vite.config.ts
+++ Lingarr.Client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ command, mode }) => {
     const isProduction = mode === 'production'
@@ -9,7 +10,10 @@ export default defineConfig(({ command, mode }) => {
         esbuild: {
             drop: isProduction ? ['console'] : []
         },
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            tailwindcss()
+        ],
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, './src')
