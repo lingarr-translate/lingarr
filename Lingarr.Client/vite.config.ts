a2e933c0036572ca59081bf86f63d2e2fd350e23
Rowan <48676736+rowanfuchs@users.noreply.github.com>
2025-02-07 15:38:43 +0100

    added tailwind plugin to vite plugins

--- Lingarr.Client/vite.config.ts
+++ Lingarr.Client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ command, mode }) => {
     const isProduction = mode === 'production'
@@ -10,10 +10,7 @@ export default defineConfig(({ command, mode }) => {
         esbuild: {
             drop: isProduction ? ['console'] : []
         },
-        plugins: [
-            vue(),
-            tailwindcss()
-        ],
+        plugins: [vue(), tailwindcss()],
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, './src')
