525d812620e8d5f5e930e68ada365bffd9cb353c
Rowan <48676736+rowanfuchs@users.noreply.github.com>
2025-02-21 10:49:02 +0100

    removed sync button from integrations

--- Lingarr.Client/src/components/features/settings/IntegrationSettings.vue
+++ Lingarr.Client/src/components/features/settings/IntegrationSettings.vue
@@ -5,7 +5,7 @@
         </template>
         <template #content>
             <SaveNotification ref="saveNotification" />
-            <div class="flex flex-col space-y-2 pb-4">
+            <div class="flex flex-col space-y-2">
                 <span class="font-semibold">
                     {{ translate('settings.integrations.radarrHeader') }}
                 </span>
@@ -24,19 +24,6 @@
                     :label="translate('settings.integrations.radarrApiKey')"
                     :error-message="translate('settings.integrations.radarrApiKeyError')"
                     @update:validation="(val) => (isValid.radarrApiKey = val)" />
-            </div>
-            <div class="flex items-center gap-2 pb-4">
-                <ButtonComponent
-                    :class="indexingMovies ? 'text-primary-content/50' : 'text-primary-content'"
-                    @click="indexMovies()">
-                    {{ translate('settings.integrations.radarrSyncButton') }}
-                </ButtonComponent>
-                <div v-if="indexingMovies" class="inline-flex overflow-hidden text-green-500">
-                    {{ translate('settings.integrations.radarrUpdating') }}
-                    <span class="animate-ellipsis">.</span>
-                    <span class="animate-ellipsis animation-delay-300">.</span>
-                    <span class="animate-ellipsis animation-delay-600">.</span>
-                </div>
             </div>
             <div class="flex flex-col space-y-2">
                 <span class="font-semibold">
@@ -58,19 +45,6 @@
                     :error-message="translate('settings.integrations.sonarrApiKeyError')"
                     @update:validation="(val) => (isValid.sonarrApiKey = val)" />
             </div>
-            <div class="flex items-center gap-2 pt-4">
-                <ButtonComponent
-                    :class="indexingShows ? 'text-primary-content/50' : 'text-primary-content'"
-                    @click="indexShows()">
-                    {{ translate('settings.integrations.sonarrSyncButton') }}
-                </ButtonComponent>
-                <div v-if="indexingShows" class="inline-flex overflow-hidden text-green-500">
-                    {{ translate('settings.integrations.sonarrUpdating') }}
-                    <span class="animate-ellipsis">.</span>
-                    <span class="animate-ellipsis animation-delay-300">.</span>
-                    <span class="animate-ellipsis animation-delay-600">.</span>
-                </div>
-            </div>
         </template>
     </CardComponent>
 </template>
@@ -78,13 +52,10 @@
 <script setup lang="ts">
 import { WritableComputedRef, computed, ref, reactive } from 'vue'
 import { useSettingStore } from '@/store/setting'
-import { useScheduleStore } from '@/store/schedule'
 import SaveNotification from '@/components/common/SaveNotification.vue'
 import { SETTINGS } from '@/ts'
 import CardComponent from '@/components/common/CardComponent.vue'
 import InputComponent from '@/components/common/InputComponent.vue'
-import ButtonComponent from '@/components/common/ButtonComponent.vue'
-import { delay } from '@/utils/delay'
 
 const isValid = reactive({
     radarrUrl: false,
@@ -92,11 +63,8 @@ const isValid = reactive({
     sonarrUrl: false,
     sonarrApiKey: false
 })
-const indexingShows = ref(false)
-const indexingMovies = ref(false)
 const saveNotification = ref<InstanceType<typeof SaveNotification> | null>(null)
 const settingsStore = useSettingStore()
-const scheduleStore = useScheduleStore()
 
 const radarrApiKey: WritableComputedRef<string> = computed({
     get: (): string => settingsStore.getSetting(SETTINGS.RADARR_API_KEY) as string,
@@ -134,20 +102,4 @@ const sonarrUrl: WritableComputedRef<string> = computed({
         }
     }
 })
-
-async function indexShows() {
-    if (indexingShows.value) return
-    indexingShows.value = true
-    await scheduleStore.indexShows()
-    await delay(5000)
-    indexingShows.value = false
-}
-
-async function indexMovies() {
-    if (indexingMovies.value) return
-    indexingMovies.value = true
-    await scheduleStore.indexMovies()
-    await delay(5000)
-    indexingMovies.value = false
-}
 </script>
