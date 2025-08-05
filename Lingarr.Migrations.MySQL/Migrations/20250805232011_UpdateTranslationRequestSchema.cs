b81d73cf19af98ebe352f5dea2c9efbd99761a4a
Zelak312 <zelak312@gmail.com>
2025-08-05 23:42:24 +0000

    Update TranslationRequest to have SubtitleToTranslate optional

---
+++ Lingarr.Migrations.MySQL/Migrations/20250805232011_UpdateTranslationRequestSchema.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lingarr.Migrations.MySQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTranslationRequestSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "subtitle_to_translate",
+                table: "translation_requests",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "translation_requests",
+                keyColumn: "subtitle_to_translate",
+                keyValue: null,
+                column: "subtitle_to_translate",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "subtitle_to_translate",
+                table: "translation_requests",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
