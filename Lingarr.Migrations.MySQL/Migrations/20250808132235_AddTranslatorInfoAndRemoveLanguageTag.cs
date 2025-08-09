fca4eb6fc9063f26b652521efa11019d3e97ec42
Rowan <48676736+rowanfuchs@users.noreply.github.com>
2025-08-09 11:57:00 +0200

    Squashed new migrations

---
+++ Lingarr.Migrations.MySQL/Migrations/20250808132235_AddTranslatorInfoAndRemoveLanguageTag.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lingarr.Migrations.MySQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTranslatorInfoAndRemoveLanguageTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "settings",
+                columns: new[] { "key", "value" },
+                values: new object[,]
+                {
+                    { "add_translator_info", "false" },
+                    { "remove_language_tag", "false" }
+                });
+            
+            migrationBuilder.AlterColumn<string>(
+                    name: "subtitle_to_translate",
+                    table: "translation_requests",
+                    type: "longtext",
+                    nullable: true,
+                    oldClrType: typeof(string),
+                    oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "settings",
+                keyColumn: "key",
+                keyValues: new object[]
+                {
+                    "add_translator_info",
+                    "remove_language_tag"
+                });
+
+            migrationBuilder.AlterColumn<string>(
+                    name: "subtitle_to_translate",
+                    table: "translation_requests",
+                    type: "longtext",
+                    nullable: false,
+                    oldClrType: typeof(string),
+                    oldType: "longtext",
+                    oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Lingarr.Migrations.SQLite/Migrations/20250808132232_AddTranslatorInfoAndRemoveLanguageTag.cs
+++ Lingarr.Migrations.SQLite/Migrations/20250808132232_AddTranslatorInfoAndRemoveLanguageTag.cs
@@ -5,11 +5,20 @@
 namespace Lingarr.Migrations.SQLite.Migrations
 {
     /// <inheritdoc />
-    public partial class UpdateTranslationRequestSchema : Migration
+    public partial class AddTranslatorInfoAndRemoveLanguageTag : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.InsertData(
+                table: "settings",
+                columns: new[] { "key", "value" },
+                values: new object[,]
+                {
+                    { "add_translator_info", "false" },
+                    { "remove_language_tag", "false" }
+                });
+            
             migrationBuilder.AlterColumn<string>(
                 name: "subtitle_to_translate",
                 table: "translation_requests",
@@ -22,6 +31,15 @@ namespace Lingarr.Migrations.SQLite.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DeleteData(
+                table: "settings",
+                keyColumn: "key",
+                keyValues: new object[]
+                {
+                    "add_translator_info",
+                    "remove_language_tag"
+                });
+            
             migrationBuilder.AlterColumn<string>(
                 name: "subtitle_to_translate",
                 table: "translation_requests",
